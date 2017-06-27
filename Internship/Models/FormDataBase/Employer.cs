3b124f9debcc05bf0b1bb186e4ca92aeba033e78
Britton Bercegeay <britton.bercegeay@selu.edu>
2017-06-27 11:51:23 -0500

    Added Classes

---
+++ Internship/Models/FormDataBase/Employer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Internship.Models
+{
+    public class Employer
+    {
+        public int EmployerId { get; set; }
+
+        [Display(Name = "First Name")]
+        public string EmployerFirstName { get; set; }
+
+        [Display(Name = "Middle")]
+        public string EmployerMiddleName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string EmployerLastName { get; set; }
+
+
+    }
+}
---
+++ Internship/Models/FormDataBase/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Internship.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public int WID { get; set; }
+        public string UserType { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Middle")]
+        public string MiddleName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+    }
+}
