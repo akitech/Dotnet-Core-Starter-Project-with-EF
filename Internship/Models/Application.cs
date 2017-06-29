30ad8189842c49f128a9560ced557108e0efffe7
Nicholas Cotton <nicholas.cotton@selu.edu>
2017-06-28 21:13:20 -0500

    ApplicationModel Properties Added

--- Internship/Models/Application.cs
+++ Internship/Models/Application.cs
@@ -9,24 +9,29 @@ namespace Internship.Models
     public class Application
     {
         [Key]
-        public int FormId { get; set; }
+        public int Id { get; set; }
+        public string Major { get; set; }
         public string Degree { get; set; }
-
         public string Concentration { get; set; }
+        public int semesterHoursEarned { get; set; }
+        public decimal majorGPA { get; set; }  //maybe change to float or something smaller than decimal? 
+        public DateTime expectededGraduationDate { get; set; }
+        public bool isPartTime { get; set; }
+        public bool isSignedByStudentApplication { get; set; }
+        public DateTime dateApplicationReceivedByyDept { get; set; }
+        public bool IsApplicationApprovedByDept { get; set; }
+        public string reasonWhyOrWhyNotApplicationIsApprovedByDept { get; set; }
+        public bool isApplicationFormSignedByInstructor { get; set; }
+        public DateTime dataInstructorSignedApplicationForm { get; set; }
+        public bool IsApplicationSignedByDeptHead { get; set; }
+        public DateTime dateApplicationsignedByDeptHead { get; set; }
+        public bool isApplicationSignedByDean { get; set; }
+        public DateTime dateApplicationSignedByDean { get; set; }
+        public enum semesterOfInternship { fall, spring, summer }
+        public bool isSignedByAcademicAdvisor { get; set; }
+        public DateTime dataSignedByAcademicAdvisor { get; set; }
+        public bool isSignedBySupervisorUponCompletion { get; set; }
+        public DataType dataSupervisorSignedUponCompletion { get; set; }
 
-        [Display(Name = "Semester Hours Earned")]
-        public int SemesterHoursEarned { get; set; }
-
-        [Display(Name = "Major GPA")]
-        public decimal MajorGPA { get; set; }
-
-        [Display(Name = "Expected Graduation Time")]
-        [DataType(DataType.Date)]
-        public DateTime ExpectedGraduationTime { get; set; }
-        public enum IsAppApproved
-        {
-            Yes,
-            No
-        }
     }
 }
